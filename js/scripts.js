77a1730d23ed059cba1137c806a3a11436d70c99
Ben Kiggen <benkiggen@gmail.com>
2018-08-24 10:29:08 -0700

    price prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 //BUSINESS LOGIC
-
-
 function Pizza(pizzaSize, pizzaToppings) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
 }
 
-
 function makePizza(pizzaSize, pizzaToppings){
   var newPizza = new Pizza(pizzaSize, pizzaToppings);
-  console.log(newPizza);
+  var finalPrice = newPizza.price().toFixed(2);
+  return finalPrice;
 }
 
-
-
-
-
-
-Pizza.prototype.prize = function() {
-
+Pizza.prototype.price = function(newPizza) {
+  var finalPrice = parseInt(this.pizzaSize) + (this.pizzaToppings.length * .5);
+  return finalPrice;
 }
 
 
@@ -31,15 +25,25 @@ Pizza.prototype.prize = function() {
 
 $(document).ready(function(){
   $("form.pizza-form").submit(function(event){
+    //input
     event.preventDefault();
     var userName = $("input#name").val();
     var pizzaSize = $("#size").val();
     var pizzaToppings = $(".toppings:checkbox:checked").map(function(){
       return $(this).val();
     }).get();
-    makePizza(pizzaSize, pizzaToppings);
+    var pizzaPrice = makePizza(pizzaSize, pizzaToppings);
+    console.log(pizzaPrice);
 
-
-    // console.log(userName + " " + pizzaSize + " " + typeof pizzaToppings);
+    //output
+    $(".result").show();
+    $(".input").hide();
+    $("#start-over").click(function(){
+      location.reload();
+    });
+    $("#make-changes").click(function(){
+      $(".input").show();
+      $(".result").hide();
+    });
   });
 });
