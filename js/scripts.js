44b8fece96077e21b6e13332804df3de7c2c31e9
Ben Kiggen <benkiggen@gmail.com>
2018-08-24 11:22:09 -0700

    display toppings list as a string, separated by commas

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,23 @@ function Pizza(pizzaSize, pizzaToppings) {
 function makePizza(pizzaSize, pizzaToppings){
   var newPizza = new Pizza(pizzaSize, pizzaToppings);
   newPizza.finalPrice = newPizza.price().toFixed(2);
+  switch(newPizza.pizzaSize){
+    case "5":
+      newPizza.pizzaSize = "Really Small";
+      break;
+    case "7":
+      newPizza.pizzaSize = "Small";
+      break;
+    case "9":
+      newPizza.pizzaSize = "Medium";
+      break;
+    case "11":
+      newPizza.pizzaSize = "Big";
+      break;
+    case "13":
+      newPizza.pizzaSize = "Really Big";
+      break;
+  };
   return newPizza;
 }
 
@@ -18,17 +35,23 @@ function makePizza(pizzaSize, pizzaToppings){
 Pizza.prototype.price = function(newPizza) {
   var finalPrice = parseInt(this.pizzaSize) + (this.pizzaToppings.length * .5);
   return finalPrice;
-}
+};
 
-
-
-
+Pizza.prototype.toppingsList = function(){
+  var listArray = [];
+  this.pizzaToppings.forEach(function(item){
+    listArray.push(item);
+  });
+  var listString = listArray.join(", ");
+  return listString;
+};
 
 
 
 //UI LOGIC
 $(document).ready(function(){
   $("form.pizza-form").submit(function(event){
+
     //input
     event.preventDefault();
     var userName = $("input#name").val();
@@ -37,18 +60,22 @@ $(document).ready(function(){
       return $(this).val();
     }).get();
     var pizzaObject = makePizza(pizzaSize, pizzaToppings);
-
-
     //output
     $(".result").show();
     $(".input").hide();
+    $("#name-output").text(userName);
     $("#final-price").text("$" + pizzaObject.finalPrice);
-    pizzaObject.pizzaToppings.forEach(function(item){
-      $("#toppings-list").append(item + ", ");
-    });
+    $("#pizza-size-output").text(pizzaObject.pizzaSize);
+    // console.log(pizzaObject);
+    var toppings = pizzaObject.toppingsList();
+      $("#pizza-toppings-output").text(toppings);
+    if(pizzaObject.pizzaToppings.length === 0) {
+      $("#pizza-toppings-output").text("None");
+    };
     $("#start-over").click(function(){
       location.reload();
     });
+
     $("#make-changes").click(function(){
       $(".input").show();
       $(".result").hide();
